f1dcf3c53135ae18584b17d0d96dfaef9aa68a3f
vpiza <vpiza@xmltravelgate.com>
2016-10-05 12:41:04 +0200

    FirstCommit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # xtg-data-sqlclient
-Sql client
+xtg-data-sqlclient is a simple library that allow connect with sql server.
+This library contain four asynchronous methods:
+- ExecAsync: execute a setence in sql server.
+- ExecQueryWithoutParamsAsync: execute a query without parameters.
+- ExecQueryWithParamsAsync: execute a query with parameters.
+- ExecProcedureDataTableWithParamsAsync: execute a stored procedure with parameters.
---
+++ XTG.Data.SqlClient/src/XTG.Data.SqlClient/DBBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace XTG.Data.SqlClient
+{
+    public class DBBase : DBLibrary
+    {
+        private string connectionString;
+        public DBBase(string bdConString)
+        {
+            connectionString = bdConString;
+        }
+
+        public async Task<int> ExecAsync(string query)
+        {
+            return await base.ExecuteNonQueryAsync(connectionString, query, new List<SqlParameter>(), CommandType.Text);
+        }
+
+        public async Task<IEnumerable<Dictionary<string, T>>> ExecQueryWithoutParamsAsync<T>(string query)
+        {
+            return await ExecQueryWithParamsAsync<T>(query, new List<SqlParameter>());
+        }
+
+        public async Task<IEnumerable<Dictionary<string, T>>> ExecQueryWithParamsAsync<T>(string query, List<SqlParameter> listParams)
+        {
+            return await base.ExecuteDataTableAsync<T>(connectionString, query, listParams, CommandType.Text);
+        }
+
+        public async Task<IEnumerable<Dictionary<string, T>>> ExecProcedureDataTableWithParamsAsync<T>(string ProcedureName, List<SqlParameter> listParams)
+        {
+            return await base.ExecuteDataTableAsync<T>(connectionString, ProcedureName, listParams, CommandType.StoredProcedure);
+        }
+
+    }
+}
